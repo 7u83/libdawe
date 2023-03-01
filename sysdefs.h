025a4cec9d607a96adfaff8ee27e06ab6bd4c2e7
7u83 <7u83@mail.ru>
2023-03-01 02:53:15 +0100

    Updateded for C++

--- sysdefs.h
+++ sysdefs.h
@@ -1,37 +1,57 @@
 #ifndef SYSDEFS_H
 #define SYSDEFS_H
 
-#if defined(__STDC__)
-# define DAWE_CVERSION 89L
-# if defined(__STDC_VERSION__)
-#  undef DAWE_CVERSION
-#  define DAWE_CVERSION 90L
-#  if (__STDC_VERSION__ >= 199409L)
+
+
+#ifndef __cplusplus
+# if defined(__STDC__)
+#  define DAWE_CVERSION 89L
+#  if defined(__STDC_VERSION__)
 #   undef DAWE_CVERSION
-#   define DAWE_CVERSION 94L
-#  endif
-#  if (__STDC_VERSION__ >= 199901L)
-#   undef DAWE_CVERSION
-#   define DAWE_CVERSION 99L
-#  endif
-#  if (__STDC_VERSION__ >= 201112L)
-#   undef DAWE_CVERSION
-#   define DAWE_CVERSION 11L
+#   define DAWE_CVERSION 90L
+#   if (__STDC_VERSION__ >= 199409L)
+#    undef DAWE_CVERSION
+#    define DAWE_CVERSION 94L
+#   endif
+#   if (__STDC_VERSION__ >= 199901L)
+#    undef DAWE_CVERSION
+#    define DAWE_CVERSION 99L
+#   endif
+#   if (__STDC_VERSION__ >= 201112L)
+#    undef DAWE_CVERSION
+#    define DAWE_CVERSION 11L
+#   endif
 #  endif
 # endif
 #endif
+
 
 #ifndef _POSIX_C_SOURCE
 # define _POSIX_C_SOURCE 199506L
 #endif
 
-#if (DAWE_CVERSION >= 99L)
-  #include <stdint.h>
+
+#define _STR(x) #x
+#define STR(x) _STR(x)
+
+
+#ifndef __cplusplus
+# if (DAWE_CVERSION >= 99L)
+#  include <stdint.h>
+# else
+   typedef unsigned char uint8_t;
+   typedef unsigned short uint16_t;
+   typedef unsigned int uint32_t;
+# define inline
+# endif
 #else
-  typedef unsigned char uint8_t;
-  typedef unsigned short uint16_t;
-  typedef unsigned int uint32_t;
-  #define inline
+# if __cplusplus > 199711L
+#  include <cstdint>
+# else
+   typedef unsigned char uint8_t;
+   typedef unsigned short uint16_t;
+   typedef unsigned int uint32_t;
+# endif
 #endif
 
 #include <arpa/inet.h>
@@ -43,7 +63,6 @@
 #ifndef letoh32
   #define letoh32(X) (ntohl(ntohl(X)))
 #endif
-
 
 #ifndef htole16
   #define htole16(X) (htons(htons(X)))
